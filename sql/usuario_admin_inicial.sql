8d441c156a736f963cac844638df42e7c41b3eda
Agustín Luzuriaga <agustin.luzuriaga103@gmail.com>
2026-08-01 09:15:59 -0300

    Agrego archivo sql que no existía

---
+++ sql/usuario_admin_inicial.sql
@@ -0,0 +1,7 @@
+INSERT INTO usuarios (usuario, password, nombre, rol, activo) VALUES (
+    'admin',
+    '210000:cy+eJ6GaQkWut8HDf3WltQ==:j4ewszlGHmyvhCGVaa9Fl7q0XTfuWrzzmv4wwbDOVBE=',
+    'Administrador',
+    'ADMINISTRADOR',
+    TRUE
+);
